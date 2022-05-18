331b87b3d362ed05e7975bc08b0799c6c614b1f6
amits <amits@harmon.ie>
2022-05-18 19:44:38 +0300

    ADD: adding authentication helper

---
+++ packages/servers/serverless/utils/authentication/index.ts
@@ -0,0 +1,31 @@
+import { IAuthenticateResponse, IConfig } from "../../entities";
+import { HttpProvider } from "../httpProvider";
+
+export class AuthenticationProvider {
+  private static readonly _httpService: HttpProvider = new HttpProvider();
+  private readonly _config: IConfig;
+  constructor(config: IConfig) {
+    this._config = config;
+  }
+  async getAppAuthenticationToken(): Promise<IAuthenticateResponse> {
+    return await AuthenticationProvider._httpService.post<IAuthenticateResponse>(
+      this._config.authenticationUrl.replace(
+        "*{tenantId}*",
+        this._config.tenantId
+      ),
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          grant_type: "client_credentials",
+          client_id: this._config.appClientId,
+          client_secret: this._config.appClientSecret,
+          resource: "20e940b3-4c77-4b0b-9a53-9e16a1b010a7",
+          scope: `https://graph.microsoft.com/.default`,
+        }).toString(),
+      }
+    );
+  }
+}
