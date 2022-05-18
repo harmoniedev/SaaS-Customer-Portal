0c13780b219689a1d1b906f3b2cb576cb268ab0f
amits <amits@harmon.ie>
2022-05-18 17:56:39 +0300

    Fix: refactor http provider

--- packages/servers/serverless/utils/httpProvider/index.ts
+++ packages/servers/serverless/utils/httpProvider/index.ts
@@ -1,35 +1,51 @@
 import fetch, { RequestInfo, RequestInit } from "node-fetch";
 
 export class HttpProvider {
-  async callApi<TRequest, TResponse>(
+  async callApi<TResponse>(
     url: RequestInfo,
     init?: RequestInit
   ): Promise<TResponse> {
     const response = await fetch(url, init);
     return (await response.json()) as TResponse;
   }
-  async get<TRequest, TResponse>(
+
+  async get<TResponse>(
     url: RequestInfo,
     init?: RequestInit
   ): Promise<TResponse> {
-    return await this.callApi(url, { method: "GET", ...(init || {}) });
+    return await this.callApi<TResponse>(url, {
+      method: "GET",
+      ...(init || {}),
+    });
   }
-  async put<TRequest, TResponse>(
+
+  async put<TResponse>(
     url: RequestInfo,
     init?: RequestInit
   ): Promise<TResponse> {
-    return await this.callApi(url, { method: "PUT", ...(init || {}) });
+    return await this.callApi<TResponse>(url, {
+      method: "PUT",
+      ...(init || {}),
+    });
   }
-  async post<TRequest, TResponse>(
+
+  async post<TResponse>(
     url: RequestInfo,
     init?: RequestInit
   ): Promise<TResponse> {
-    return await this.callApi(url, { method: "POST", ...(init || {}) });
+    return await this.callApi<TResponse>(url, {
+      method: "POST",
+      ...(init || {}),
+    });
   }
-  async delete<TRequest, TResponse>(
+
+  async delete<TResponse>(
     url: RequestInfo,
     init?: RequestInit
   ): Promise<TResponse> {
-    return await this.callApi(url, { method: "DELETE", ...(init || {}) });
+    return await this.callApi<TResponse>(url, {
+      method: "DELETE",
+      ...(init || {}),
+    });
   }
 }
