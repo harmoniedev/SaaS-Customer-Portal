4a754acc2f82225cab74dbf2ddcbb29c15ebd891
amits <amits@harmon.ie>
2022-05-24 19:58:16 +0300

    change APP_TENANT_ID name in config

--- packages/servers/serverless/utils/configuration/index.ts
+++ packages/servers/serverless/utils/configuration/index.ts
@@ -3,7 +3,7 @@ import { DbTypes, IConfig } from "../../entities";
 const resolveSubscriptionEndPoint =
   process.env.RESOLVE_SUBSCRIPTION_API_END_POINT;
 const authenticationUrl = process.env.MICROSOFT_LOGIN_END_POINT;
-const tenantId = process.env.TENANT_ID;
+const tenantId = process.env.APP_TENANT_ID;
 const appClientId = process.env.APP_CLIENT_ID;
 const subscriptionBaseUrl = process.env.FULFILLMENT_BASE_URL;
 const appClientSecret = process.env.APP_CLIENT_SECRET;
