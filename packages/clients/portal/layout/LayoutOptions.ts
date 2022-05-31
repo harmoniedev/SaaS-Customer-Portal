56cd513dbed3fea4690836747d504bdb015bec48
Liliia Chuiko <chuiko1112@gmail.com>
2022-05-31 09:32:50 +0100

    test the layout

--- packages/clients/portal/layout/LayoutOptions.ts
+++ packages/clients/portal/layout/LayoutOptions.ts
@@ -1,4 +1,5 @@
 export const menuItems = [
   { title: 'Dashboard', icon: null, id: 'dashboard' },
   { title: 'Support', icon: null, id: 'support' },
+  { title: 'Test LP', icon: null, id: 'landing-page-for-customer-portal' },
 ];
