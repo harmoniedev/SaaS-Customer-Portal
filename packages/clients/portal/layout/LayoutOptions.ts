0471b388d7563c90cb23730e3918aa4f5816767a
Liliia Chuiko <chuiko1112@gmail.com>
2022-06-06 13:20:14 +0100

    renamed tab

--- packages/clients/portal/layout/LayoutOptions.ts
+++ packages/clients/portal/layout/LayoutOptions.ts
@@ -1,5 +1,5 @@
 export const menuItems = [
+  { title: 'Dowload', icon: null, id: 'landing-page-for-customer-portal' },
   { title: 'Dashboard', icon: null, id: 'dashboard' },
   { title: 'Support', icon: null, id: 'support' },
-  { title: 'Test LP', icon: null, id: 'landing-page-for-customer-portal' },
 ];
