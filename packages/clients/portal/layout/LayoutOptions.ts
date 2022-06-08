9af3907ff44a5b44b71b6fcbd552842038ef144a
Liliia Chuiko <chuiko1112@gmail.com>
2022-06-08 14:59:09 +0100

    test the outside pages functionality

--- packages/clients/portal/layout/LayoutOptions.ts
+++ packages/clients/portal/layout/LayoutOptions.ts
@@ -1,5 +1,5 @@
-export const menuItems = [
-  { title: 'Download', icon: null, id: 'landing-page-for-customer-portal' },
-  { title: 'Dashboard', icon: null, id: 'dashboard' },
-  { title: 'Support', icon: null, id: 'support' },
+export const defaultMenuItems = [
+  // { title: 'Download', icon: null, id: 'landing-page-for-customer-portal' },
+  { label: 'Dashboard', key: "1", external: '/dashboard', isSideMenu: true },
+  { label: 'Support', key: "2", external: '/support', isSideMenu: true },
 ];
