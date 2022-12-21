ad4bf3c0aa71e360cafd67d281612983ab5a3b75
Amitai Biton <amitaib@harmon.ie>
2022-12-21 12:28:25 +0200

    remove project

--- components/form/FormOptions.ts
+++
@@ -1,8 +0,0 @@
-export const roleList = ['Member', 'Owner'];
-
-export const departmentList = [
-  'Research and Development',
-  'Marketing',
-  'Accounting and Finance',
-  'Production',
-];
--- helpers/utils/removeHref.ts
+++
@@ -1,6 +0,0 @@
-export const removeHref = (href = '') =>
-  href
-    .replace(/\.en\.html/g, '')
-    .replace(/\.fr\.html/g, '')
-    .replace(/\.html/g, '')
-    .replace(/html\//g, '');
