003fb089a14f9ef746b2ab48ac55083aabd1d7ef
Amitai Biton <amitaib@harmon.ie>
2023-01-11 21:42:54 +0200

    fixed jwt token  issue

--- helpers/utils/jwt.ts
+++ helpers/utils/jwt.ts
@@ -1,10 +1,11 @@
-export const parseJWT = (token) => {
-    try {
-        const parsed = JSON.parse(atob(token.split('.')[1]));
+export function parseJWT(token)
+{
+     var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 
-        return parsed;
-    } catch (e) {
-        return 'error'
-    }
-};
+    return JSON.parse(jsonPayload);
 
+}
